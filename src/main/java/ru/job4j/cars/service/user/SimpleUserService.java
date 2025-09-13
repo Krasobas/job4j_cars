d5e251e2392b9a95fc3bc7050191026940d8e58f
v.krasov.ext <v.krasov@aleria.cloud>
2025-09-13 19:22:41 +0400

    Update UserService to use PasswordEncoder for bcrypt

--- src/main/java/ru/job4j/cars/service/user/SimpleUserService.java
+++ src/main/java/ru/job4j/cars/service/user/SimpleUserService.java
@@ -2,6 +2,7 @@ package ru.job4j.cars.service.user;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.jbosslog.JBossLog;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.job4j.cars.dto.user.UserCreateDto;
 import ru.job4j.cars.dto.user.UserSessionDto;
@@ -17,17 +18,35 @@ import java.util.Optional;
 public class SimpleUserService implements UserService {
     private final UserRepository repository;
     private final UserMapper mapper;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public boolean save(UserCreateDto user) {
         User entity = mapper.getEntityOnCreate(user);
+        entity.setPassword(passwordEncoder.encode(user.getPassword()));
         return repository.persist(entity);
     }
 
     @Override
     public Optional<UserSessionDto> findByEmailAndPassword(String email, String password) {
-        return repository.findByEmailAndPassword(email, password)
-                .map(mapper::getDto);
+        log.info("Attempting login for email: " + email);
+        Optional<User> user = repository.findByEmail(email);
+
+        if (user.isPresent()) {
+            User foundUser = user.get();
+            log.info("User found, checking password for: " + email);
+
+            if (passwordEncoder.matches(password, foundUser.getPassword())) {
+                log.info("Password matches for: " + email);
+                return Optional.of(mapper.getDto(foundUser));
+            } else {
+                log.warn("Password mismatch for: " + email);
+            }
+        } else {
+            log.warn("User not found: " + email);
+        }
+
+        return Optional.empty();
     }
 
     @Override
